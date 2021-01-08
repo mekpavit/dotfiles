d6ca21f6bf4beb591378b9ce10e6ef8bf5965a44
mek.kiatkrai <pavit.kiat@gmail.com>
2021-01-08 19:47:29 +0700

    update doom config

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -99,13 +99,30 @@
                                    :env nil
                                    :envFile nil)))
 
-(defun go-coverage-all ()
+(defun my/go-coverage-all ()
   "Show go coverage of current buffer."
   (interactive)
   (shell-command "go test -tags=integration -coverprofile=cover.out ./...")
   (go-coverage "cover.out"))
 
+;; override Doom's function
+(defun my/go-test-single ()
+  (interactive)
+  (if (string-match "_test\\.go" buffer-file-name)
+      (save-excursion
+        (re-search-backward "^func[ ]+\\(([[:alnum:]]*?[ ]?[*]?[[:alnum:]]+)[ ]+\\)?\\(Test[[:alnum:]_]+\\)(.*)")
+        (+go--run-tests (concat "-tags=integration " "-run" "='" (match-string-no-properties 2) "'")))
+    (error "Must be in a _test.go file")))
+
+;; override Doom's function
+(defun my/go-test-all ()
+  (interactive)
+  (+go--run-tests "-tags=integration"))
+
 (map! :map go-mode-map
       :localleader
       (:prefix ("c" . "coverage")
-       "a" #'go-coverage-all))
+       "a" #'go-coverage-all
+       :prefix ("t" . "test")
+       "s" #'my/go-test-single
+       "a" #'my/go-test-all))
