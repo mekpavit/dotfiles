3594de61a17a5aebc20fc5c306f4640d83475cd8
mekpavit <pavit140239@gmail.com>
2024-01-07 12:40:28 +0700

    add vimwiki and taskwarrior-int

---
+++ dot_config/nvim/lua/plugins/vimwiki.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    "vimwiki/vimwiki",
+    keys = {
+      { "<leader>ww", "<cmd>VimwikiIndex<cr>" },
+      { "<C-space>", "<cmd>VimwikiToggleListItem<cr>" },
+    },
+    init = function(_)
+      vim.g.vimwiki_list = { { path = "~/vimwiki", syntax = "markdown", ext = ".md" } }
+      vim.g.vimwiki_ext2syntax = { [".md"] = "markdown", [".markdown"] = "markdown", [".mdown"] = "markdown" }
+      vim.g.vimwiki_markdown_link_ext = 1
+      vim.g.taskwiki_markdown_syntax = "markdown"
+    end,
+  },
+  { "ribelo/taskwarrior.nvim", config = true },
+  { "tools-life/taskwiki" },
+}
